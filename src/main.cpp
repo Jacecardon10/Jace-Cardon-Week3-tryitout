09ded473104abb2bcd914f9076bf67fc6ada699d
Jacec <jacecardon@mail.weber.edu>
2026-01-20 18:56:20 -0700

    Week 3 try it out

--- src/main.cpp
+++ src/main.cpp
@@ -15,3 +15,31 @@ If I was to type 3 it would show
 * * * *
 
 */
+
+#include <iostream>
+#include <string>
+
+
+int main() {
+    int pyramidHeight = 0;
+	std:: cout << "How tall do you want the pyramid to be? ";
+    std::cin >> pyramidHeight;
+
+    //Main iteration for each row
+    for (int row = 1; row <= pyramidHeight; row++) {
+        //Calculate empty spaces before the first star in each row
+        int emptySpace = (pyramidHeight + 1) - row;
+
+        //Puts the correct amount of empty space before each star
+        for (int i = 1; i < emptySpace; i++) {
+            std::cout << "  ";
+        }
+        //Places stars
+        for (int star = 1; star < row *2; star++) {
+            std::cout << " *";
+        }
+        //Starts new row
+        std::cout << " \n";
+    }
+    return 0;
+}
